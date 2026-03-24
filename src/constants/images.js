182890d56db440b45ef2a04c6478190ded9b9fc7
DANNT <danthenguyen92@gmail.com>
2026-03-24 16:43:49 +0900

    delete all

--- src/constants/images.js
+++
@@ -1,9 +0,0 @@
-import arrow from "../assets/arrow-down.png";
-import logo from "../assets/logo.png";
-import edit from "../assets/edit.svg";
-
-export default {
-    arrow,
-    logo,
-    edit,
-}
